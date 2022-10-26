a88e214b8343e37fd862338490f12052c597d96a
guilhermesmatiola <guimatiola@gmail.com>
2022-10-26 11:14:22 -0300

    insere carga horaria

--- insere-dados.sql
+++ insere-dados.sql
@@ -3,5 +3,5 @@ insert into semestre (id, fase) values (1, 'Primeira');
 insert into semestre (id, fase) values (2, 'Segunda');
 
 --disciplinas
-insert into disciplina (id,nome,codigo,fk_semestre_id) values (1, 'Calculo 1', 'BLU6001', 1);
-insert into disciplina (id,nome,codigo,fk_semestre_id) values (2, 'Introducao a informatica', 'BLU3101', 1);
+insert into disciplina (id,nome,codigo,fk_semestre_id, carga_horaria) values (1, 'Calculo 1', 'BLU6001', 1, 108);
+insert into disciplina (id,nome,codigo,fk_semestre_id, carga_horaria) values (2, 'Introducao a informatica', 'BLU3101', 1, 72);
