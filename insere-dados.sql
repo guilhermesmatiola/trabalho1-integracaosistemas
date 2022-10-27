64796b524941c153e11cdde62466a36d24d3718f
guilhermesmatiola <guimatiola@gmail.com>
2022-10-27 20:29:04 -0300

    add disciplinas e topicos e ementas

--- insere-dados.sql
+++ insere-dados.sql
@@ -1,6 +1,7 @@
 --semestre
 insert into semestre (id, fase) values (1, 'Primeira');
 insert into semestre (id, fase) values (2, 'Segunda');
+insert into semestre (id, fase) values (3, 'Terceira');
 insert into semestre (id, fase) values (6, 'Sexta');
 
 --disciplinas
@@ -9,6 +10,9 @@ insert into disciplina (id,nome,codigo,fk_semestre_id, carga_horaria) values (2,
 insert into disciplina (id,nome,codigo,fk_semestre_id, carga_horaria) values (3, 'Calculo 2', 'BLU6004', 2, 108);
 insert into disciplina (id,nome,codigo,fk_semestre_id, carga_horaria) values (4, 'Metodologia Eng Automação', 'BLU3706', 6, 72);
 insert into disciplina (id,nome,codigo,fk_semestre_id, carga_horaria) values (5, 'Metodologia Eng Têxtil', 'BLU1026', 6, 72);
+insert into disciplina (id,nome,codigo,fk_semestre_id, carga_horaria) values (6, 'Geometria Analitica', 'BLU6905', 1, 72);
+insert into disciplina (id,nome,codigo,fk_semestre_id, carga_horaria) values (7, 'Calculo 3', 'BLU6008', 3, 72);
+
 
 --topicos
 insert into topicos (id, descricao) values (1, 'Limites');
@@ -17,6 +21,7 @@ insert into topicos (id, descricao) values (3, 'Integrais');
 insert into topicos (id, descricao) values (4, 'Fazer referencias');
 insert into topicos (id, descricao) values (5, 'Aprender a usar MoreUFSC');
 insert into topicos (id, descricao) values (6, 'Outro software para criar referencia');
+insert into topicos (id, descricao) values (7, 'Matrizes');
 
 --ementa
 insert into ementa (fk_disciplina_id,fk_topicos_id) values (1, 1);
@@ -29,6 +34,8 @@ insert into ementa (fk_disciplina_id,fk_topicos_id) values (4, 5);
 insert into ementa (fk_disciplina_id,fk_topicos_id) values (4, 6);
 insert into ementa (fk_disciplina_id,fk_topicos_id) values (5, 4);
 insert into ementa (fk_disciplina_id,fk_topicos_id) values (5, 5);
+insert into ementa (fk_disciplina_id,fk_topicos_id) values (7, 3);
+insert into ementa (fk_disciplina_id,fk_topicos_id) values (6, 7);
 
 --pre_requisito
 insert into pre_requisito (fk_disciplina_id, fk_disciplina_id_) values (1,3)
